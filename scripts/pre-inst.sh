5cc656e6d17c0a9847a9a69d5515d71ad04211ce
tobiasvogel <tobiasvogel@users.noreply.github.com>
2019-10-10 14:39:27 +0200

    pre-inst.sh script drafted

---
+++ scripts/pre-inst.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+## Make sure we are running as root
+
+if [ ! "xʼid -uʼ" == "x0" ]; then
+
+  echo "This script needs to be run as root user!" 1>&2
+
+  exit 1
+
+fi
+
+
+## Make sure we don't run twice
+
+if [ -f "/usr/bin/raspistill.vendor" ]; then
+
+  echo "A file called \"raspistill.vendor\" already exists!" 1>&2
+  echo "This script should not be run multiple times." 1>&2
+
+  exit 1
+
+fi
+
+
+## Move the original raspistill-executable out of the way
+
+mv -nv /usr/bin/raspistill /usr/bin/raspistill.vendor
+
+
+## Make sure the raspistill-executable was moved away
+
+if [ -f /usr/bin/raspistill ]; then
+
+  echo "Unexpected \"raspistill\" still present!" 1>&2
+  echo "Something went wrong. Aborting" 1>&2
+
+  exit 1
+
+fi
+
+
+## Install 'Alternative' for "raspistill"
+
+update-alternatives --install /usr/bin/raspistill raspistill /usr/bin/raspistill.vendor 500
+update-alternatives --install /usr/bin/raspistill raspistill /usr/bin/raspistill.emulator 1000
+
+update-alternatives --set raspistill /usr/bin/raspistill.emulator
+
+exit 0
